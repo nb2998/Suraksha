857a0f881e31975ca847b14f5b435b63c0a16252
Nishtha Bhatia <nbhatia329@gmail.com>
2018-09-29 06:20:39 +0530

    Police adapter UI

--- app/src/main/java/com/codingblocks/suraksha/PoliceActivity.java
+++ app/src/main/java/com/codingblocks/suraksha/PoliceActivity.java
@@ -18,10 +18,15 @@ public class PoliceActivity extends AppCompatActivity {
 
         ArrayList<PoliceDetail> policeDetails = new ArrayList<>();
         policeDetails.add(new PoliceDetail("Deputy Comissioner Of Police", "01127034873"));
+        policeDetails.add(new PoliceDetail("Police station Karol Bagh", "01128720482"));
+        policeDetails.add(new PoliceDetail("Police station Rajendra Nagar", "0112874012"));
+        policeDetails.add(new PoliceDetail("Police station Paharganj", "01123524746"));
+        policeDetails.add(new PoliceDetail("Police station Connought Place", "01123747100"));
         policeDetails.add(new PoliceDetail("DCP Office, Delhi Police", "100"));
         policeDetails.add(new PoliceDetail("Anti crime cell @CCPM West and Outer District", "08882111255"));
         policeDetails.add(new PoliceDetail("Khyala Police", "08750871123"));
         policeDetails.add(new PoliceDetail("Police Station Punjabi Bagh", "0112554162"));
+
 
         Police adapter = new Police(policeDetails, getApplicationContext());
         RecyclerView recView = findViewById(R.id.recViewPolice);
